4c16fc3edbdbf4842b8a348a1fd5780b0983c540
Dashon Howard <i@dashon.co>
2020-06-23 22:06:21 -0500

    add webhook logic

--- app/Http/Controllers/Api/WebHooksController.php
+++ app/Http/Controllers/Api/WebHooksController.php
@@ -9,6 +9,8 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use AlexVargash\LaravelStripePlaid\StripePlaid;
+use App\Donation;
+use App\Transaction;
 use Stripe\Stripe;
 
 class WebHooksController extends Controller
@@ -34,40 +36,61 @@ class WebHooksController extends Controller
 
         $event = null;
         if ($endpoint_secret) {
-          try {
-            $event = \Stripe\Webhook::constructEvent(
-                $request,
-                $sig_header,
-                $endpoint_secret
-            );
-          } catch (\Exception $e) {
-            return response([ 'error' => $e->getMessage() ],403);
-          }
+            try {
+                $event = \Stripe\Webhook::constructEvent(
+                    $request,
+                    $sig_header,
+                    $endpoint_secret
+                );
+            } catch (\Exception $e) {
+                return response(['error' => $e->getMessage()], 403);
+            }
         } else {
 
-          $event = $request;
+            $event = $request;
         }
         $type = $event['type'];
         $object = $event['data']['object'];
 
         if ($type == 'payment_intent.succeeded') {
-        //  $logger->info('🔔 A SetupIntent has successfully set up a PaymentMethod for future use.');
-        print_r($object);
-        return response(['result'=>$object->metadata],200);
-         }
+            //  $logger->info('🔔 A SetupIntent has successfully set up a PaymentMethod for future use.');
+            if ($object->metadata->donation_id) {
+                $donation = Donation::where('id', $object->metadata->donation_id)->get();
+                if ($donation->status == 'payment_failed') {
+                    $donation->status = "active";
+                    $donation->save();
+                }
+
+                $transaction = Transaction::where('stripe_payment_intent', $object->id)->get();
+                $transaction->status = $object->status;
+                $transaction->save();
+
+                return response(['result' => "updated Transaction"], 200);
+            }
+        }
 
         if ($type == 'payment_method.attached') {
-        //  $logger->info('🔔 A PaymentMethod has successfully been saved to a Customer.');
+            //  $logger->info('🔔 A PaymentMethod has successfully been saved to a Customer.');
 
-          // At this point, associate the ID of the Customer object with your
-          // own internal representation of a customer, if you have one.
+            // At this point, associate the ID of the Customer object with your
+            // own internal representation of a customer, if you have one.
         }
 
         if ($type == 'payment_intent.payment_failed') {
+            if ($object->metadata->donation_id) {
+                $donation = Donation::where('id', $object->metadata->donation_id)->get();
+                if ($donation->status == 'active') {
+                    $donation->status = "payment_failed";
+                    $donation->save();
+                }
 
+                $transaction = Transaction::where('stripe_payment_intent', $object->id)->get();
+                $transaction->status = $object->status;
+                $transaction->save();
+                return response(['result' => "updated Transaction"], 200);
+            }
         }
 
-        return response([ 'status' => 'success' ],200);
+        return response(['status' => 'success'], 200);
     }
-
 }
