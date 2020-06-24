35847d2f0f42b4b3abb423bccce400b9590c5a4d
Dashon Howard <i@dashon.co>
2020-06-24 10:18:55 -0500

    test

--- app/Http/Controllers/Api/WebHooksController.php
+++ app/Http/Controllers/Api/WebHooksController.php
@@ -55,7 +55,7 @@ class WebHooksController extends Controller
         if ($type == 'payment_intent.succeeded') {
             //  $logger->info('🔔 A SetupIntent has successfully set up a PaymentMethod for future use.');
             if ($object->metadata->donation_id) {
-                $donation = Donation::where('id', $object->metadata->donation_id)->get();
+                $donation = Donation::where('id', $object->metadata->donation_id);
                echo $object->metadata->donation_id;
                 dd($donation);
                 if ($donation->status == 'payment_failed') {
